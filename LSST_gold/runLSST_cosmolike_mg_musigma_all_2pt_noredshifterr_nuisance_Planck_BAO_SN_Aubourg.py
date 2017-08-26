ce765292478a2f4d2a126f7999ccefe44ba9ca79
Hironao Miyatake <hironao.miyatake.p@gmail.com>
2017-08-26 15:07:59 -0700

    Add rest of run script.

---
+++ LSST_gold/runLSST_cosmolike_mg_musigma_all_2pt_noredshifterr_nuisance_Planck_BAO_SN_Aubourg.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+#sys.path.append('/Users/teifler/Dropbox/cosmolike/top-level/miyatake/')
+
+from cosmolike_libs import * 
+
+file_source_z = os.path.join(dirname, "../../zdistris/zdistribution_LSST")
+file_lens_z = os.path.join(dirname, "../../zdistris/zdistribution_LSST_gold")
+data_file = os.path.join(dirname, "datav/LSST_all_2pt_fid")
+cov_file = os.path.join(dirname, "cov/Multi_Probe_LSST_2.600000e+01_1.800000e+04_Rmin10_lmin30_lens-zmin0.15LSST_cov_Ncl20_Ntomo10_2pt_inv")
+chain_file = os.path.join(dirname, "./like/like_LSST_all_2pt_noredshifterr_nuisance_Planck_BAO_SN_Aubourg_zmin0.15_smallerstd")
+
+update_Ntable_Na(20)
+initcosmo()
+initbins(20,30.0,1000.0,1000.0,10.0,7)
+initsurvey("LSST")
+initgalaxies(file_source_z,file_lens_z,"gaussian","gaussian","gold")
+initclusters()
+initia("none","DEEP2")
+initpriors("Planck_BAO_SN_Aubourg","none","none","none")
+#initprobes("shear_shear")
+initprobes("all_2pt")
+initdatainv(cov_file ,data_file)
+initclusters()
+update_nuisance_LSST_source()
+
+#sample_params= sample_cosmology_only(MG = True)
+#sample_params = sample_cosmology_shear_nuisance(get_N_tomo_shear())
+sample_params = sample_cosmology_2pt_nuisance(get_N_tomo_shear(),get_N_tomo_clustering(), MG = True)
+#sample_params = sample_cosmology_2pt_nuisance_IA_marg(get_N_tomo_shear(),get_N_tomo_clustering())
+#sample_params = sample_cosmology_2pt_cluster_nuisance(get_N_tomo_shear(),get_N_tomo_clustering()) 
+sample_main(sample_params,16000,128,24,chain_file, blind=False)
---
+++ LSST_gold/runLSST_cosmolike_mg_musigma_pos_pos_noredshifterror.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+#sys.path.append('/Users/teifler/Dropbox/cosmolike/top-level/miyatake/')
+
+from cosmolike_libs_muOnly import * 
+
+file_source_z = os.path.join(dirname, "../../zdistris/zdistribution_LSST")
+file_lens_z = os.path.join(dirname, "../../zdistris/zdistribution_LSST_gold")
+data_file = os.path.join(dirname, "datav/LSST_pos_pos_fid")
+cov_file = os.path.join(dirname, "cov/Multi_Probe_LSST_2.600000e+01_1.800000e+04_Rmin10_lmin30_lens-zmin0.15LSST_cov_Ncl20_Ntomo10_clustering_inv")
+chain_file = os.path.join(dirname, "./like/like_LSST_pos_pos_noredshifterr_zmin0.15")
+
+update_Ntable_Na(20)
+initcosmo()
+initbins(20,30.0,1000.0,1000.0,10.0,7)
+initsurvey("LSST")
+initgalaxies(file_source_z,file_lens_z,"gaussian","gaussian","gold")
+initclusters()
+initia("none","DEEP2")
+initpriors("none","none","none","none")
+initprobes("pos_pos")
+initdatainv(cov_file ,data_file)
+initclusters()
+update_nuisance_LSST_source()
+
+sample_params= sample_cosmology_only(MG = True)
+#sample_params = sample_cosmology_shear_nuisance(get_N_tomo_shear())
+#sample_params = sample_cosmology_2pt_nuisance(get_N_tomo_shear(),get_N_tomo_clustering())
+#sample_params = sample_cosmology_2pt_nuisance_IA_marg(get_N_tomo_shear(),get_N_tomo_clustering())
+#sample_params = sample_cosmology_2pt_cluster_nuisance(get_N_tomo_shear(),get_N_tomo_clustering()) 
+sample_main(sample_params,4000,64,24,chain_file, blind=False)
